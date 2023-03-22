ecf1b097586dded217eb3b203ea8dadbc3d0f31a
qiqa1212 <tumishgo@gmail.com>
2023-03-22 14:06:46 +0200

    Шаблони + дебаг

--- forums/urls.py
+++ forums/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .import views
-app_name = 'learning_logs'
+
+
+app_name = 'forums'
+
+
+
 urlpatterns = [
      path('', views.index, name='index'),
+     path('topics/', views.topics, name='topics'),
+     path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
-     return render(request, 'learning_logs/index.html')
+     return render(request, 'forums/index.html')
+
+
+def topics(request):
+     topics = Topic.objects.order_by('date_added')
+     context = {'topics': topics}
+     return render(request, 'forums/topics.html', context)   
+
+def topic(request, topic_id):
+     topic = Topic.objects.get(id=topic_id)
+     entries = topic.entry_set.order_by('-date_added')
+     context = {'topic': topic, 'entries': entries}
+     return render(request, 'forums/topic.html', context)
