e1948a09ea76a25831b69961bb13be6bec993439
qiqa1212 <tumishgo@gmail.com>
2023-03-28 17:55:21 +0300

    add bootstrap4 to index, login, base

--- forum/settings.py
+++ forum/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'forums',
     'users',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
